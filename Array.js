e2e8f38b2bb95b8a9fb5a6e890b06d9c17ad450c
Mayu_pr <mayurirane56@gmail.com>
2026-06-26 10:30:18 +0530

    UPDATED

--- Array.js
+++ Array.js
@@ -9,9 +9,10 @@ const MyNewArr = ["Mayuri", "Pradip", "Rane"];
 // console.log(newArr);
 //we can create array using new keyword
 const arr1 = new Array(5, 4, 7, 9, 2);
-console.log(arr1);
-console.log(arr1.length);
-console.log(arr1.slice(0, 3)); //doesnot change original array
-console.log(arr1);
-console.log(arr1.splice(0, 3)); //changes original array
-console.log(arr1);
+// console.log(arr1);
+// console.log(arr1.length);
+// console.log(arr1.slice(0, 3)); //doesnot change original array
+// console.log(arr1);
+// console.log(arr1.splice(0, 3)); //changes original array
+// console.log(arr1);
+console.log(Array.from("Mayuri")); //convert into array
