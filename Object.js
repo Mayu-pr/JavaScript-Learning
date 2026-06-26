f6301b82e44f89047164ce5b831b2a180f6c482f
Mayu_pr <mayurirane56@gmail.com>
2026-06-26 12:31:38 +0530

    Object basic done

---
+++ Object.js
@@ -0,0 +1,38 @@
+//we can declare obj 1)literal-> not singleton(multiple instance)
+//                   2)constructor ->singleton using constructor
+//Object.create ->singleton using constr
+//obj literals
+const mySym = Symbol("key");
+const JsUser = {
+  name: "Mayuri",
+  [mySym]: "key1",
+  age: 29,
+  location: "Pune",
+  lastLoginDays: ["Monday", "Saturday"],
+};
+// console.log(JsUser.name);
+// console.log(JsUser["name"]);
+console.log(JsUser[mySym]);
+
+JsUser.greeting = function () {
+  console.log(`Hello Js User, ${JsUser.name}`);
+};
+
+// console.log(JsUser.greeting());
+// --------------------------------------------------------------
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+//const obj3 = Object.assign({}, obj1, obj2);
+const obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+//const tinderUser = new Object(); this is singleton
+const tinderUser = {};
+tinderUser.id = "1";
+tinderUser.email = "someone@gmail.com";
+tinderUser.isLoggedIn = true;
+
+console.log(tinderUser);
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+console.log(tinderUser.hasOwnProperty("isLogged"));
